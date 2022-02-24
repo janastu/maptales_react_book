7df37ff5d2a6c3540b68c3109fff0937ec67d6bf
salus-sage <salus.sage@gmail.com>
2022-02-24 16:37:17 +0530

    remove browser router and add hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { HashRouter, BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 
